fc6fb4fdc7de2c322b2072767739f0ebe9a9fd3b
Ahmed <abutt@digitalocean.com>
2021-07-05 21:19:34 +0000

    Django App Portfolio

---
+++ jobs/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Job(models.Model):
+    # IMAGES
+    image = models.ImageField(upload_to='images/')
+    # SUMMARY
+    summary = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.summary
