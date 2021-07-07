b9ba4b0cbb9bed74c5a387ff9ddc415058fc80d0
Ahmed <abutt@digitalocean.com>
2021-07-07 23:59:08 +0000

    editing models and settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -126,7 +126,7 @@ MEDIA_ROOT = BASE_DIR
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_DIRS = (os.path.join(BASE_DIR),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
