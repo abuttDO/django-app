6624d53a9639769766924bb68c599972cd70cfdd
Ahmed <abutt@digitalocean.com>
2021-07-08 01:05:02 +0000

    make changes

--- jobs/models.py
+++ jobs/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Job(models.Model):
     # IMAGES
-    image = models.ImageField(upload_to='staticfiles/')
+    image = models.ImageField(upload_to='static/')
     # SUMMARY
     summary = models.CharField(max_length=500)
 
