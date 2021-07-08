6828ea2beda462af592526517ed8457993b744ab
Ahmed <abutt@digitalocean.com>
2021-07-08 02:13:48 +0000

    adding new models named project

--- jobs/models.py
+++ jobs/models.py
@@ -6,6 +6,8 @@ class Job(models.Model):
     image = models.ImageField(upload_to='static/')
     # SUMMARY
     summary = models.CharField(max_length=500)
+    # Project Type
+    project = models.CharField(max_length=200)
 
     def __str__(self):
         return self.summary
