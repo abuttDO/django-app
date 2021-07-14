64f9abb9af930503d627381b2be925ffbe46c9bb
Ahmed <abutt@digitalocean.com>
2021-07-14 14:38:15 +0000

    trying tutorial code

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -147,16 +147,20 @@ AWS_S3_OBJECT_PARAMETERS = {
 #STATIC_URL = 'https://%s/%s/' % (AWS_S3_ENDPOINT_URL, 'static')
 #STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-STATICFILES_LOCATION = 'static'
-STATICFILES_STORAGE = 'custom_storages.StaticStorage'
-
-MEDIAFILES_LOCATION = 'media'
-DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
 #MEDIAFILES_DIRS = [
 #    os.path.join(BASE_DIR, 'jobs/media'),
 #]
 #MEDIA_URL = 'https://%s/%s/' % (AWS_S3_ENDPOINT_URL, 'media')
 #MEDIAFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+########### LINKEDIN TUTORIAL SITE CODE ################
+STATICFILES_LOCATION = 'static'
+STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+
+MEDIAFILES_LOCATION = 'media'
+DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+########################################################
+
 ###################################
 
 
