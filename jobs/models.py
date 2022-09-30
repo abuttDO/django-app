fcf4dc0bd0e42d633e822363c0537e72dfc0c41f
John David Back <johndavidback@gmail.com>
2022-09-30 10:09:25 -0400

    Change in model and adding environ for local testing

--- jobs/models.py
+++ jobs/models.py
@@ -3,9 +3,11 @@ from django.db import models
 # Create your models here.
 class Job(models.Model):
     # IMAGES
-    image = models.ImageField(upload_to='static/')
+    image = models.ImageField(upload_to='media/job_images/')
     # SUMMARY
+
     summary = models.CharField(max_length=500)
+    
     # Project Type
     #project = models.CharField(max_length=200)
 
